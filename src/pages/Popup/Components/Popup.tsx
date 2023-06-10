a8e5e0dc081f66a3c0791ebafffa5176322c63e8
Yuichi Kageyama <yuichi.kageyama@outlook.com>
2023-06-10 21:38:31 +1000

    some styling

--- src/pages/Popup/Components/Popup.tsx
+++ src/pages/Popup/Components/Popup.tsx
@@ -1,50 +1,61 @@
-import { useState } from 'react';
-import { logger } from '@/helpers/logger';
-import { FiCalendar } from 'react-icons/fi';
-import { CalendarModal } from './Calendar';
-import { dateExists } from '@/helpers/date';
-import dayjs from 'dayjs';
+import {useState} from 'react'
+import {logger} from '@/helpers/logger'
+import {FiCalendar} from 'react-icons/fi'
+import {CalendarModal} from './Calendar'
+import {dateExists} from '@/helpers/date'
+import dayjs from 'dayjs'
 import {LoadingOverlay} from './LoadingOverlay'
 import {RecordList} from './Record'
 import {Footer} from './Footer'
 import {Toaster} from 'react-hot-toast'
-const today = new Date();
+const today = new Date()
 
-
+export interface RecordItem {
+  from: {
+    hour?: string
+    min?: string
+  }
+  to: {
+    hour?: string
+    min?: string
+  }
+  workType?: string
+  timeType?: string
+}
 
 const Popup = () => {
-  const [dates, setDates] = useState<Date[]>([today]);
-  const [showCalendar, setShowCalendar] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
+  const [dates, setDates] = useState<Date[]>([today])
+  const [showCalendar, setShowCalendar] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
 
   const handleCalendarSubmit = (d: Date[]) => {
-    setShowCalendar(false);
-    setDates(d);
-  };
+    setShowCalendar(false)
+    setDates(d)
+  }
   const getHeaderText = () => {
-    if (dates.length === 0) return 'No days selected';
+    if (dates.length === 0) return 'No days selected'
 
-    const parts = [];
-    const includesToday = dateExists(dates, today);
+    const parts = []
+    const includesToday = dateExists(dates, today)
     const includesYesterday = dateExists(
       dates,
       new Date(dayjs(today).subtract(1, 'day').toDate())
-    );
+    )
 
     const addYesterday =
-      includesYesterday && (!includesToday || dates.length === 2);
+      includesYesterday && (!includesToday || dates.length === 2)
 
-    if (includesToday) parts.push('Today');
-    if (addYesterday) parts.push('Yesterday');
+    if (includesToday) parts.push('Today')
+    if (addYesterday) parts.push('Yesterday')
     if (!(addYesterday && includesToday) && dates.length >= 1) {
-      const subDays = 2 - (includesToday ? 1 : 0) - (addYesterday ? 1 : 0);
+      const subDays = 2 - (includesToday ? 1 : 0) - (addYesterday ? 1 : 0)
       const otherDayCount =
-        includesToday || addYesterday ? dates.length - subDays : dates.length;
+        includesToday || addYesterday ? dates.length - subDays : dates.length
       if (otherDayCount)
-        parts.push(`${otherDayCount} ${otherDayCount > 1 ? 'days' : 'day'}`);
+        parts.push(`${otherDayCount} ${otherDayCount > 1 ? 'days' : 'day'}`)
     }
-    return parts.join(' + ');
-  };
+    return parts.join(' + ')
+  }
 
   const onFill = () => {
     setIsLoading(true)
@@ -57,28 +68,28 @@ const Popup = () => {
 
   return (
     <div>
-      {isLoading && <LoadingOverlay/>}
+      {isLoading && <LoadingOverlay />}
       <CalendarModal
         initialDates={dates}
         show={showCalendar}
         setShow={setShowCalendar}
         onSubmit={(dates) => handleCalendarSubmit(dates)}
       />
-      <Toaster position='bottom-center' toastOptions={{duration: 2000, }}/>
+      <Toaster position='bottom-center' toastOptions={{duration: 2000, }} />
       <div>
-        <div className="flex gap-2 items-bottom mx-4 mt-4">
-          <p className="text-2xl font-bold pl-5 text-rose-400">
+        <div className="flex gap-2 mx-4 mt-4 items-bottom">
+          <p className="pl-5 text-2xl font-bold text-rose-400">
             {getHeaderText()}
           </p>
           <button tabIndex={-1} className="text-xl" onClick={() => setShowCalendar(true)}>
             <FiCalendar />
           </button>
         </div>
-        <RecordList/>
+        <RecordList />
       </div>
-      <Footer handleFill={onFill}/>
+      <Footer handleFill={onFill} />
     </div>
-  );
-};
+  )
+}
 
-export default Popup;
+export default Popup
--- src/pages/Popup/Components/Record.tsx
+++ src/pages/Popup/Components/Record.tsx
@@ -1,63 +1,63 @@
 import {DropdownSelect} from '@/components/DropdownSelect'
 import {logger} from '@/helpers/logger'
-import { ChangeEvent, useRef, useState, forwardRef } from 'react';
-import {toast} from 'react-hot-toast'
-import { FaPlus } from 'react-icons/fa';
+import {ChangeEvent, useRef, useState, forwardRef} from 'react'
+import {FaPlus, FaTrash} from 'react-icons/fa'
+import {RecordItem} from './Popup'
 
 
 interface Time {
-  hour: string;
-  min: string;
+  hour: string
+  min: string
 }
 
 interface TimePickerProps {
-  nextRef?: React.MutableRefObject<HTMLInputElement | null>;
+  nextRef?: React.MutableRefObject<HTMLInputElement | null>
 }
 
 const TimePicker = forwardRef<HTMLInputElement, TimePickerProps>(({nextRef}, ref) => {
-  const [time, setTime] = useState<Time>({ hour: '', min: '' });
-  const minTimeRef = useRef<HTMLInputElement>(null);
+  const [time, setTime] = useState<Time>({hour: '', min: ''})
+  const minTimeRef = useRef<HTMLInputElement>(null)
 
   const onHourChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
+    const value = e.target.value
 
-    if (value.length > 2) return;
+    if (value.length > 2) return
     if (value.length === 0) {
-      setTime({ ...time, hour: '' });
-      return;
+      setTime({...time, hour: ''})
+      return
     }
 
-    const parsed = parseInt(value);
-    if (isNaN(parsed)) return;
-    const clamped = '' + Math.min(Math.max(parsed, 0), 23);
+    const parsed = parseInt(value)
+    if (isNaN(parsed)) return
+    const clamped = '' + Math.min(Math.max(parsed, 0), 23)
     const hour = clamped.padStart(value.length, '0')
-    setTime({ ...time, hour});
+    setTime({...time, hour})
     if (hour.length === 2) {
-      minTimeRef.current?.focus();
+      minTimeRef.current?.focus()
     }
-  };
+  }
 
   const onMinChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
-    if (value.length > 2) return;
+    const value = e.target.value
+    if (value.length > 2) return
     if (value.length === 0) {
-      setTime({ ...time, min: '' });
-      return;
+      setTime({...time, min: ''})
+      return
     }
 
-    const parsed = parseInt(e.target.value);
-    if (isNaN(parsed)) return;
-    const clamped = '' + Math.min(Math.max(parsed, 0), 59);
+    const parsed = parseInt(e.target.value)
+    if (isNaN(parsed)) return
+    const clamped = '' + Math.min(Math.max(parsed, 0), 59)
     const min = clamped.padStart(value.length, '0')
 
-    setTime({ ...time, min});
+    setTime({...time, min})
     if (min.length === 2) {
       logger.log(nextRef)
-      nextRef?.current?.focus();
+      nextRef?.current?.focus()
     }
-  };
+  }
   return (
-    <div className="focus-within:border-rose-300  rounded-full bg-stone-50 flex border border-stone-200 h-9">
+    <div className="flex border rounded-full focus-within:border-rose-300 bg-stone-50 border-stone-200 h-9">
       <input
         ref={ref}
         placeholder="00"
@@ -66,9 +66,9 @@ const TimePicker = forwardRef<HTMLInputElement, TimePickerProps>(({nextRef}, ref
         tabIndex={2}
         type="text"
         value={time.hour}
-        className="w-12 text-center overflow-hidden bg-transparent focus:outline-none"
+        className="w-12 overflow-hidden text-center bg-transparent focus:outline-none"
       />
-      <span className="font-bold text-lg">:</span>
+      <span className="text-lg font-bold">:</span>
       <input
         placeholder="00"
         ref={minTimeRef}
@@ -77,10 +77,10 @@ const TimePicker = forwardRef<HTMLInputElement, TimePickerProps>(({nextRef}, ref
         tabIndex={2}
         type="text"
         value={time.min}
-        className="w-12 text-center overflow-hidden bg-transparent focus:outline-none"
+        className="w-12 overflow-hidden text-center bg-transparent focus:outline-none"
       />
     </div>
-  );
+  )
 })
 
 const timeTypes = [
@@ -92,7 +92,7 @@ const timeTypes = [
     label: 'took a break',
     value: 'Break',
   },
-];
+]
 
 const workTypes = [
   {
@@ -103,15 +103,15 @@ const workTypes = [
     label: 'OPEX',
     value: 'OPEX'
   },
-];
+]
 
-const Record = () => {
-  const [timeType, setTimeType] = useState<typeof timeTypes[number]['value']>();
+const Record = ({onRemove}: {onRemove: () => void}) => {
+  const [timeType, setTimeType] = useState<typeof timeTypes[number]['value']>()
 
-  const toRef = useRef(null);
-  const workTypeRef = useRef<typeof workTypes[number]['value'] | null>(null);
+  const toRef = useRef(null)
+  const workTypeRef = useRef<typeof workTypes[number]['value'] | null>(null)
   const handleWorkTypeSelect = (workType: typeof workTypes[number]['value']) => {
-    workTypeRef.current = workType;
+    workTypeRef.current = workType
   }
 
   const handleTimeTypeSelect = (tt: typeof timeTypes[number]['value']) => {
@@ -120,64 +120,59 @@ const Record = () => {
 
 
   return (
-    <div className="text-lg font-bold flex flex-col gap-2">
-      <div className="gap-2 flex justify-start items-center ">
-        I <DropdownSelect onSelect={handleTimeTypeSelect} options={timeTypes} placeHolder='worked'/>
+    <div className="flex flex-col gap-2 text-lg font-bold">
+      <div className="flex items-center gap-2">
+        I <DropdownSelect onSelect={handleTimeTypeSelect} options={timeTypes} placeHolder='worked' />
+        <button className="transition-all group ml-auto p-2 rounded-full hover:bg-stone-200" onClick={onRemove}>
+          <FaTrash className="w-4 h-4 text-rose-400 hover group-hover:text-rose-500 transition-all " />
+        </button>
       </div>
 
-      <div className="flex justify-start items-center  gap-2">
-        From <TimePicker nextRef={toRef}/>
-        To <TimePicker ref={toRef}/>
+      <div className="flex items-center justify-start gap-2">
+        From <TimePicker nextRef={toRef} />
+        To <TimePicker ref={toRef} />
       </div>
-      {timeType === 'Normal Time' && <div className="flex justify-start items-center  gap-2">
-        Doing <DropdownSelect onSelect={handleWorkTypeSelect} options={workTypes} placeHolder='CAPEX'/> work
-      </div>}
-    </div>
-  );
-};
+      {
+        timeType === 'Normal Time' && <div className="flex items-center justify-start gap-2">
+          Doing <DropdownSelect onSelect={handleWorkTypeSelect} options={workTypes} placeHolder='CAPEX' /> work
+        </div>
+      }
+    </div >
+  )
+}
 
 export const RecordList = () => {
   const [records, setRecords] = useState<RecordItem[]>([])
 
   const handleAddRecord = () => {
-    toast('hello')
     const prev = records[records.length - 1]
     setRecords([...records, {
       from: {
-        hour: prev?.to.hour ?? '00',
-        min: prev?.to.min ?? '00',
+        hour: prev?.to.hour,
+        min: prev?.to.min,
       },
       to: {
       },
     }])
+  }
 
+  const handleRemoveRecord = (index: number) => {
+    setRecords(records.filter((_, i) => i !== index))
   }
 
   return (
     <div className="flex flex-col gap-2 h-[450px] overflow-y-auto overflow-x-hidden p-2 scrollbar-thin scrollbar-thumb-rose-300 scrollbar-track-transparent">
       {records.map((record, i) => (
-        <div className="text-base p-4 bg-stone-400/10 rounded-2xl">
-          <Record />
+        <div className="p-4 text-base bg-stone-400/10 rounded-2xl">
+          <Record onRemove={() => handleRemoveRecord(i)} />
         </div>
       ))}
 
-      <button onClick={handleAddRecord} className="group gap-2 w-full flex justify-center items-center bg-rose-200 hover:bg-rose-300 transition-all duration-300 hover:ring ring-rose-200 hover:font-bold p-4 rounded-full mt-2 text-base">
-        <FaPlus className=" w-5 h-5 text-rose-500 group-hover:text-rose-700 transition-colors duration-300" />
+      <button onClick={handleAddRecord} className="flex items-center justify-center w-full gap-2 p-4 mt-2 text-base transition-all duration-300 rounded-full group bg-rose-200 hover:bg-rose-300 hover:ring ring-rose-200 hover:font-bold">
+        <FaPlus className="w-5 h-5 transition-colors duration-300 text-rose-500 group-hover:text-rose-700" />
         <span>Record Time</span>
       </button>
     </div>
-  );
+  )
 };
 
-interface RecordItem {
-  from: {
-    hour?: string;
-    min?: string;
-  };
-  to: {
-    hour?: string;
-    min?: string;
-  };
-  workType?: string;
-  timeType?: string;
-}
